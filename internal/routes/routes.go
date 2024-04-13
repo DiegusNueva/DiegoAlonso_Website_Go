c861f9cf14fb75cdaab593369ed8b38345bf4031
Diego <diegusnueva@gmail.com>
2024-04-13 21:22:15 +0200

    Matices en las rutas: pequeños errores de sintaxis

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"net/http"
+
+	"github.com/DiegusNueva/DiegoAlonso_Website_Go/internal/handlers"
 )
 
 func RegisterRoutes() {
-	http.HandleFunc("/", handlers.homeHandler)
-	http.HandleFunc("/", handlers.errorHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/error", handlers.ErrorHandler)
 
 	fs := http.FileServer(http.Dir("web/static"))
 
