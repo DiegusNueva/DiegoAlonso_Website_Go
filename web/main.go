17a158947faf40574ca9d401d995054ec6c6eb42
Diego <diegusnueva@gmail.com>
2024-04-13 17:24:27 +0200

    Aplicamos air a nuestro proyecto y conformamos el home.html y el error.html

--- web/main.go
+++ web/main.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/products", productsHandler)
-	http.HandleFunc("/products/detail", productDetailHandler)
+
+	fs := http.FileServer(http.Dir("web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join("web/templates/", "home.html"))
+	})
+
+	http.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join("web/templates/", "error.html"))
+	})
 
 	fmt.Println("Servidor corriendo en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Bienvenido a la página de inicio")
-}
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Aquí se muestran todos los productos disponibles")
-}
-func productDetailHandler(w http.ResponseWriter, r *http.Request) {
-	productID := r.URL.Path[len("/products/detail/"):]
-	fmt.Fprintf(w, "Detalles del producto con ID: %s", productID)
 }
