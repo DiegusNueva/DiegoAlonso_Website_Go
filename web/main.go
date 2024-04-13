0096ba7303c5f64eccaa35a624a54dda63be07e3
Diego <diegusnueva@gmail.com>
2024-04-13 12:51:25 +0200

    Implementando el primer web server

---
+++ web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hola, vamos bien")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Servidor está corriendo en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+}
